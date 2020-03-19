ed77e046f9f1854e23334547ce26a7c6ae6e43ba
Philip Lindh <philip.lindh98@hotmail.com>
2020-03-19 20:17:09 +0100

    better gif

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ build_options = {"packages": ["src", "os", "pygame", "pytmx"], "include_files": 
 bdist_options = {'add_to_path': False, 'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % ("LTU", "Strategic AI Philip Lindh")}
 exe_options = Executable(script="main.py", targetName="S0006D Strategic AI", base="Win32GUI", icon="resources/icon/Game.ico")
 
-setup(name = "S0006D Pathfinding",
+setup(name = "S0006D Strategic AI Philip Lindh",
       version = "1.0",
       description = "Author: Philip Lindh",
       options = {"build_exe": build_options,
